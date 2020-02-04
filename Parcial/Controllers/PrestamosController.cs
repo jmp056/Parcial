8806240c679b56584be88683e8da90608ff523cd
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-04 00:07:33 -0400

    Terminando el Controlador de la entidad Prestamos

---
+++ Parcial/Controllers/PrestamosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Parcial.DAL;
+using Parcial.Entidades;
+
+namespace Parcial.Controllers
+{
+    public class PrestamosController
+    {
+        public bool Guardar(Prestamos Prestamo)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Prestamos.Add(Prestamo);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+    }
+}
