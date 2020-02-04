ed8173f2cb09a5eaea5c8dfb553ff754a228f01c
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-03 23:56:33 -0400

    Terminando el contexto!

---
+++ Parcial/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = JMP-PC\SQLEXPRESS; DataBase = Parcial; Trusted_Connection = true");
+        }
+    }
+}
