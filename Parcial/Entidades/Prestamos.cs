8becfaf6c78614fa9921b0588062ffa2d9a9de54
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-04 00:35:30 -0400

    Agregando las validaciones a la entidad!

--- Parcial/Entidades/Prestamos.cs
+++ Parcial/Entidades/Prestamos.cs
@@ -10,10 +10,16 @@ namespace Parcial.Entidades
     {
         [Key]
         public int PrestfloamoId { get; set; }
+        [Required(ErrorMessage ="El campo nombre no puede estar vacio!...")]
         public string Nombre { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: 9999999999999, ErrorMessage ="Debe ingresar un monto valido")]
         public float Capital { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: 999999, ErrorMessage = "Debe ingresar un monto valido")]
         public int Semanas { get; set; }
         public float TotalAPagar { get; set; }
+        [Required]
         public DateTime FechaPrestamo { get; set; }
 
         public Prestamos()
