ea148ad3ae9e4c7b5615bbd6bfbdce054a4df183
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-03 23:58:00 -0400

    Agregando la migracion al proyecto

---
+++ Parcial/Migrations/20200204035648_Inicial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    PrestfloamoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true),
+                    Capital = table.Column<float>(nullable: false),
+                    Semanas = table.Column<int>(nullable: false),
+                    FechaPrestamo = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.PrestfloamoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+        }
+    }
+}
