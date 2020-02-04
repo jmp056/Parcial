afe15372c85d59345322f3aaaf42c076b6b3362a
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-03 23:52:44 -0400

    Terminando la entidad Prestamos

---
+++ Parcial/Entidades/Prestamos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial.Entidades
+{
+    public class Prestamos
+    {
+        [Key]
+        public int PrestfloamoId { get; set; }
+        public string Nombre { get; set; }
+        public float Capital { get; set; }
+        public int Semanas { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+
+        public Prestamos()
+        {
+            PrestfloamoId = 0;
+            Nombre = string.Empty;
+            Capital = 0;
+            Semanas = 0;
+            FechaPrestamo = DateTime.Now;
+        }
+    }
+}
